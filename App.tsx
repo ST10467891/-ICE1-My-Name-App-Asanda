2069e0e84e01760a8506e1e3b0052850e43c2209
Asanda Twala <ST10467891>
2025-07-31 16:51:44 +0200

    initial code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const[userInput,setUserInput] = useState('');
+  const handlePress = () => {
+    console.log('User Input:' , userInput)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>HELLO, ASANDA!</Text>
+      <TextInput
+       placeholder="HI, Viewer what's your name? Enter here please!"
+       value={userInput}
+       onChangeText={setUserInput}
+      />
+      <Button 
+      title="Click Me"
+      onPress={handlePress}
+      />
+      <Text>user name verified</Text>
+    
     </View>
   );
 }
@@ -13,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#800020a',
     alignItems: 'center',
     justifyContent: 'center',
   },
